a81cb7be8a00a85e25b17071974db2c036663f73
Sandy Jones <sandynashv@gmail.com>
2016-03-14 21:26:30 -0500

    iife completion

--- Predator.js
+++ Predator.js
@@ -1,6 +1,7 @@
 "use strict";
-
+//predator is a global variable so loadCarnivores is a property within the returned object of predator
 let Predator = (function () {
+  let herbivores = [];
   let carnivores = [];
   return {
   	//another way to write a function
@@ -11,22 +12,37 @@ let Predator = (function () {
       // loader.open tells loader where to go
       loader.open("GET", "carnivores.json");
       // loader.send methods of xhr request xml-http request send executes
-      loader.send();
-
+      loader.send();  //send is sending the json file
       loader.addEventListener("load", function () {
         // Set the value of the private array
-        carnivores = JSON.parse(this.responseText);
+        console.log(this.responseText); 
+        carnivores = JSON.parse(this.responseText).carnivores;//turning json into an object that javascript can manipulate
+        //turns json string into an object
         console.log(carnivores);
-
-  cb(carnivores);
+      cb(carnivores);
         // Invoke the callback function so that the caller knows
         // that the process is complete. Make sure to pass the 
         // carnivore array as an argument.
-
+      });
+    },
+    //another way to write a function
+    loadHerbivores: function (callB) {
+      //setting up a constructor function XML etc
+      //request is defined by variable loader
+      let loaderH = new XMLHttpRequest();
+      // loader.open tells loader where to go
+      loaderH.open("GET", "herbivores.json");
+      // loader.send methods of xhr request xml-http request send executes
+      loaderH.send();
+      loaderH.addEventListener("load", function () {
+        // Set the value of the private array
+        herbivores = JSON.parse(this.responseText).herbivores;
+        console.log(herbivores);
+        // Invoke the callback function so that the caller knows
+        // that the process is complete. Make sure to pass the 
+        // carnivore array as an argument.
+      callB(herbivores);
       });
     }
-  }
+   } 
 })();
-
-//do the herbivores only diffe is in the return
-//loadHerbivores instead of cb(carnivores)
--- iife-xhr.js
+++ iife-xhr.js
@@ -1,10 +1,23 @@
 "use strict";
-function showCarnivores (carnivores) {
-	console.log(carnivores);
-}
-
-function showHerbivores (herbivores) {
-  console.log(herbivores);
+let list = document.getElementById("carnivores-list");
+let outputString = "";
+function showCarnivores(carnivores) {
+   for (let i = 0; i < carnivores.length; i++) {
+      let currentCarnivore = carnivores[i];
+      outputString += `<h1>${currentCarnivore.name}</h1>`;
+      outputString += `<h3>${currentCarnivore.family}</h3>`;
+      list.innerHTML += outputString;
+      }
+    }
+function showHerbivores(herbivores) {
+  let list = document.getElementById("herbivores-list");
+     for (let i = 0; i < herbivores.length; i++) {
+       let currentHerbivore = herbivores[i];
+       outputString += `<h1>${currentHerbivore.name}</h1>`;
+       outputString += `<h3>${currentHerbivore.family}</h3>`;
+       list.innerHTML += outputString;
+      }
 }
 //showCarnivores is the callback function
 Predator.loadCarnivores(showCarnivores);
+Predator.loadHerbivores(showHerbivores);
